1d82a526384b1c622fae7cfd56b7ece568be11a8
LuckyNoS7evin <andy.ramit@gmail.com>
2020-08-13 21:18:23 +0100

    started enum summary

--- src/S7evemetry.Core/Enums/DriverStatus.cs
+++ src/S7evemetry.Core/Enums/DriverStatus.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace S7evemetry.Core.Enums
+﻿namespace S7evemetry.Core.Enums
 {
     /// <summary>
     /// Status of driver - 0 = in garage, 1 = flying lap, 
@@ -10,10 +6,29 @@ namespace S7evemetry.Core.Enums
     /// </summary>
     public enum DriverStatus
     {
+        /// <summary>
+        /// Driver is in the garage, value 0
+        /// </summary>
         InGarage = 0,
+
+        /// <summary>
+        /// Driver is on their flying lap, value 1
+        /// </summary>
         FlyingLap = 1,
+
+        /// <summary>
+        /// Driver is on their in lap, value 2
+        /// </summary>
         InLap = 2,
+
+        /// <summary>
+        /// Driver is on their out lap, value 3
+        /// </summary>
         OutLap = 3,
+
+        /// <summary>
+        /// Driver is on track, value 4
+        /// </summary>
         OnTrack = 4
     }
 }
--- src/S7evemetry.Core/Enums/DrsAllowed.cs
+++ src/S7evemetry.Core/Enums/DrsAllowed.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace S7evemetry.Core.Enums
+﻿namespace S7evemetry.Core.Enums
 {
     /// <summary>
-    /// 0 = not allowed, 1 = allowed, -1 = unknown
+    /// The status of DRS
     /// </summary>
     public enum DrsAllowed
     {
+        /// <summary>
+        /// DRS is not allowed, value 0
+        /// </summary>
         NotAllowed = 0,
+
+        /// <summary>
+        /// DRS is allowed, value 1
+        /// </summary>
         Allowed = 1,
+
+        /// <summary>
+        /// DRS has unknown state, value -1
+        /// </summary>
         Unknown = -1,
     }
 }
